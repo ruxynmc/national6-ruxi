05658d61d3d870cf5682683242cd0927619cc8d3
Ruxi Coman <ruxandra_coman@yahoo.com>
2021-03-23 19:25:50 +0200

    recap

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("JavaScript OOP Game");
+
+// object literal
+// this comes from Object class
+const obj = {};
+
+const a = new Object();
+console.log(a);
+
+// this comes from Array class
+const b = [];
+
+class Car {
+    constructor() {
+        this.color = "black";
+    }
+
+    starCar() {}
+}
+
+const car1 = new Car("white");
+console.log(car1);
